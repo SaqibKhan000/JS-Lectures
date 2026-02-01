38eb1bfd8a48898c5134d02fc865a512c0f9152c
M Saqib Khan <saqibkhannn543@gmail.com>
2026-02-01 17:05:28 +0500

    JS Array method part(1)

---
+++ JSArrayMethodPart(1)/script.js
@@ -0,0 +1,49 @@
+
+
+
+// 1️⃣ push() → end (last) mai item add karta hai
+
+// let fruits = ["Apple", "Banana"];
+
+// fruits.push("Mango");
+
+// console.log(fruits);
+
+
+
+
+
+
+
+// 2️⃣ pop() → end (last) se item remove karta hai
+
+// let fruits = ["Apple", "Banana", "Mango"];
+
+// fruits.pop();
+
+// console.log(fruits);
+
+
+
+
+
+
+// 3️⃣ unshift() → start (first) mai item add karta hai
+
+// let fruits = ["Banana", "Mango"];
+
+// fruits.unshift("Apple");
+
+// console.log(fruits);
+
+
+
+
+
+
+// 4️⃣ shift() → start (first) se item remove karta hai
+// let fruits = ["Apple", "Banana", "Mango"];
+
+// fruits.shift();
+
+// console.log(fruits);
