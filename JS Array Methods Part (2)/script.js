d5eff83180689292187d6d331688b6eb1c3a605f
M Saqib Khan <saqibkhannn543@gmail.com>
2026-02-04 19:02:20 +0500

    JS Array Methods part:2

---
+++ JS Array Methods Part (2)/script.js
@@ -0,0 +1,23 @@
+
+
+
+///   Slice method  ///
+
+// const arr = [1, 2, 3, 4];
+// const result = arr.slice(1, 3);
+
+// console.log(result); // [2, 3]
+// console.log(arr);    // [1, 2, 3, 4]
+
+
+
+
+
+
+
+///   Splice method  ///
+
+// const arr = [1, 2, 3, 4];
+// arr.splice(1, 2, 'a');
+
+// console.log(arr); // [1, 'a', 4]
