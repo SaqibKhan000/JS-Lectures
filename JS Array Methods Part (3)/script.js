56fcfa2e533a9dc22d6087b26b8be02407dd6169
M Saqib Khan <saqibkhannn543@gmail.com>
2026-02-07 17:31:13 +0500

    JS Array Method Part(3)

---
+++ JS Array Methods Part (3)/script.js
@@ -0,0 +1,85 @@
+
+
+
+// 1️⃣ flat()
+
+// Nested array ko single array bana deta hai.
+
+// const arr = [1, 2, [3, 4]];
+// console.log(arr.flat());
+
+// Output:
+// [1, 2, 3, 4]
+
+
+
+
+
+
+
+
+
+
+
+
+// 2️⃣ sort()
+
+// Array ke elements ko sort karta hai (by default string ke hisaab se).
+
+// const numbers = [10, 2, 5];
+// numbers.sort();
+// console.log(numbers);
+
+// Output:
+// [10, 2, 5]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 3️⃣ includes()
+
+// Check karta hai ke value array mai exist karti hai ya nahi.
+
+// const fruits = ["apple", "banana", "mango"];
+// console.log(fruits.includes("banana"));
+
+
+// Output:
+// true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 4️⃣ join()
+
+// Array ko string mai convert kar deta hai.
+
+// const words = ["Hello", "World"];
+// console.log(words.join(" "));
+
+
+// Output:
+// Hello World
