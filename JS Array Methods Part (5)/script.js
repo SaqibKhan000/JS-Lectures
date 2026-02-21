b1f021d23630b79089b8c66fa121caa1ba1b1ef8
M Saqib Khan <saqibkhannn543@gmail.com>
2026-02-21 22:01:13 +0500

    JS Array Methods Part (5)

---
+++ JS Array Methods Part (5)/script.js
@@ -0,0 +1,47 @@
+
+// 🔹 forEach() Method – Definition
+
+// The forEach() method executes a provided function once for each element in an array. It does not return a new array and is mainly used for performing side effects such as logging, updating values, or making API calls.
+
+// 🌍 Real Life Example
+
+// Socho aap ek class teacher ho aur sirf attendance announce karni hai.
+
+// const students = ["Ali", "Sara", "Ahmed"];
+
+// students.forEach(student => {
+//   console.log(student + " is present");
+// });
+
+
+
+
+
+
+
+
+
+
+// 🔹 map() Method – Definition
+
+// The map() method creates a new array by applying a provided function to every element of the original array. It returns a new array containing the transformed values
+
+// 🌍 Real Life Example
+
+// Socho aap ke paas product prices hain:
+
+// const prices = [100, 200, 300];
+
+// Aur aap 10% discount apply karna chahte ho:
+
+// const discounted = prices.map(price => price * 0.9);
+
+// console.log(discounted);
+// // [90, 180, 270]
+
+
+
+
+
+
+
